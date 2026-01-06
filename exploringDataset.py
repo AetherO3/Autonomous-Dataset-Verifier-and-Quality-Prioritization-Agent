770d4d6b2516abc7364d2514e251c6db6c47fdbf
AetherO3 <utkristhtopwal@gmail.com>
2026-01-07 00:32:32 +0530

    Added feature to check for active links.

--- exploringDataset.py
+++ exploringDataset.py
@@ -1,8 +1,10 @@
+import requests 
 import pandas as pd
 import numpy as np
 
 
 # link to another dataset ** ---- https://amazon-reviews-2023.github.io/#quick-start ---- **
+# dataset used ** ---- https://www.kaggle.com/datasets/karkavelrajaj/amazon-sales-dataset ---- **
 df = pd.read_csv('amazon.csv') # Kaggle Dataset
 
 print(df.shape, "\n")
@@ -28,3 +30,26 @@ df.dropna(axis = 0)
 df['discount_percentage'] = (df["discount_percentage"].astype(str).str.replace(r"[^\d.]","", regex = True).astype(float))
 
 print(df['discount_percentage'].mean())
+
+# Finding the active links
+
+sample = df.sample(25, random_state = 0)
+
+result = []
+
+for idx, url in df["img_link"].items():
+    try:
+        res = requests.get(url, timeout = .1)
+        stat = (res.status_code == 200) and ("image" in res.headers.get("Content-Type", ""))
+        result.append((idx, int(stat)))
+        print(idx , "\n")
+    
+    except:
+        result.append((idx, 0))
+
+df["img_available"] = 0
+for idx, stat in result:
+    df.loc[idx, "img_available"] = stat
+
+success = sum(stat for _, stat in result)
+print(f"{success} of 25 images were accessible.")
