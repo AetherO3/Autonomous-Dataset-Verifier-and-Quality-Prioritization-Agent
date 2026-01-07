a013f65d06771955022004a1b7ab6c329fd0ccad
AetherO3 <utkristhtopwal@gmail.com>
2026-01-08 00:11:34 +0530

    fixed the object type to more apt types

--- exploringDataset.py
+++ exploringDataset.py
@@ -23,13 +23,28 @@ missingPerc = df.isna().mean() * 100
 print("The percentage of mising values in each columns is : \n\n",round(missingPerc, 2))
 
 
-# Finding noise and inconsistencies
+# Finding noise and inconsistencies and fixing the datatypes
 
-df.dropna(axis = 0)
+df = df.dropna(axis = 0)
 
-df['discount_percentage'] = (df["discount_percentage"].astype(str).str.replace(r"[^\d.]","", regex = True).astype(float))
+df["product_id"] = (df["product_id"].astype(str))
+df["product_name"] = (df["product_name"].astype(str))
+df["category"] = (df["category"].astype(str))
 
-print(df['discount_percentage'].mean())
+#   for numerical data, conversion to float
+df["discounted_price"] = (df["discounted_price"].astype(str).str.replace(r"[^\d.]","", regex = True).astype(float))
+df["actual_price"] = (df["actual_price"].astype(str).str.replace(r"[^\d.]","", regex = True).astype(float))
+df["discount_percentage"] = (df["discount_percentage"].astype(str).str.replace(r"[^\d.]","", regex = True).astype(float))
+df["rating"] = (df["rating"].astype(str).str.replace(r"[^\d.]","", regex = True).replace("", np.nan).astype(float))
+df["rating_count"] = (df["rating_count"].astype(str).str.replace(r"[^\d.]","", regex = True).replace("", np.nan).astype(float))
+
+df["about_product"] = (df["about_product"].astype(str))
+df["user_id"] = (df["user_id"].astype(str))
+df["user_name"] = (df["user_name"].astype(str))
+df["review_id"] = (df["review_id"].astype(str))
+df["review_title"] = (df["review_title"].astype(str))
+df["img_link"] = (df["img_link"].astype(str))
+df["product_link"] = (df["product_link"].astype(str))
 
 # Finding the active links
 
@@ -37,9 +52,9 @@ sample = df.sample(25, random_state = 0)
 
 result = []
 
-for idx, url in df["img_link"].items():
+for idx, url in sample["img_link"].items():
     try:
-        res = requests.get(url, timeout = .1)
+        res = requests.get(url, timeout = 1)
         stat = (res.status_code == 200) and ("image" in res.headers.get("Content-Type", ""))
         result.append((idx, int(stat)))
         print(idx , "\n")
@@ -53,3 +68,5 @@ for idx, stat in result:
 
 success = sum(stat for _, stat in result)
 print(f"{success} of 25 images were accessible.")
+
+print(sample.dtypes)
